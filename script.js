33787833188f2126b1448390fd0609942e34ce8b
Abdulhakeem Zakariyau <zakariyauabdulhakeem32@gmail.com>
2025-09-02 20:24:13 +0100

    delete button added

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
+// To display mathematical expressions
 function addToDisplay(value) {
   document.getElementById('outPut').value += value;
 }
 
+// To compute the result
 function compute() {
   let result = eval(document.getElementById('outPut').value);
   document.getElementById('outPut').value = result;
 }
 
+// To delete the last character
+function deleteLast() {
+  let outPut = document.getElementById('outPut')
+  outPut.value = outPut.value.slice(0, -1)
+}
+
+// To clear the display
 function clearValues() {
   document.getElementById('outPut').value = ('');
 }
